1babf854fd41d33902722cd20db594bc00b7842b
MrMatin-Aka-Me <matindzhuraev@gmail.com>
2021-09-03 20:05:50 +0300

    v2, added post editing/safe deleting/getById/restoring functions

--- main.js
+++ main.js
@@ -28,11 +28,27 @@ function sendJSON(response, body) {
 
 const methods = new Map();
 methods.set('/posts.get', function({response}){
-    sendJSON(response, posts);
+    const savedPosts = posts.filter(post => !post.removed);
+    sendJSON(response, savedPosts);
 });
 
 methods.set('/posts.getById', function({response, searchParams}){
-    
+    if (!searchParams.has('id') || isNaN(Number(searchParams.get('id')))){
+        sendResponse(response, {status: statusBadRequest});
+        return;
+    } 
+    // else if (searchParams.get('id').isNaN()){
+    // }
+    const id = Number(searchParams.get('id'));
+    // console.log(id);
+    for (const post of posts) {
+        // console.log(post.id);
+        if (!post.removed && post.id === id){
+            sendJSON(response, post);
+            return;
+        }
+    }
+    sendResponse(response, {status: statusNotFound});
 });
 
 methods.set('/posts.post', function({response, searchParams}){
@@ -46,15 +62,70 @@ methods.set('/posts.post', function({response, searchParams}){
         id: nextId++,
         content: content,
         created: Date.now(),
+        removed: false,
     };
 
     posts.unshift(post);
     sendJSON(response, post);
 });
 
-methods.set('/posts.edit', function(request, response){});
+methods.set('/posts.edit', function({response, searchParams}){
+    if (!searchParams.has('id') || isNaN(Number(searchParams.get('id'))) || !searchParams.has('content')){
+        sendResponse(response, {status: statusBadRequest});
+        return;
+    }
 
-methods.set('/posts.delete', function(request, response){});
+    const id = Number(searchParams.get('id'));
+    const updatedContent = searchParams.get('content');
+    
+    for (const post of posts) {
+        if (!post.removed && post.id === id){
+            post.content = updatedContent;
+            sendJSON(response, post);
+            return;
+        }
+    }
+    sendResponse(response, {status: statusNotFound});
+});
+
+methods.set('/posts.delete', function({response, searchParams}){
+    if (!searchParams.has('id') || isNaN(Number(searchParams.get('id')))){
+        sendResponse(response, {status: statusBadRequest});
+        return;
+    }
+    const id = Number(searchParams.get('id'));
+    const postIndex = posts.findIndex((post) => post.id === id);
+    if (postIndex !== -1 && !posts[postIndex].removed){
+        const post = posts[postIndex];
+        post.removed = true;
+        sendJSON(response, post);
+        return;
+    }
+    sendResponse(response, {status: statusNotFound});
+});
+
+methods.set('/posts.restore', function({response, searchParams}){
+    if (!searchParams.has('id') || isNaN(Number(searchParams.get('id')))){
+        sendResponse(response, {status: statusBadRequest});
+        return;
+    }
+
+    const id = Number(searchParams.get('id'));
+    const postIndex = posts.findIndex((post) => post.id === id);
+    if (postIndex !== -1){
+        const post = posts[postIndex];
+
+        if (!post.removed){
+            sendResponse(response, {status: statusBadRequest});
+            return;
+        }
+
+        post.removed = false;
+        sendJSON(response, post);
+        return;
+    }
+    sendResponse(response, {status: statusNotFound});
+});
 
 const server = http.createServer((request, response) => {
     const {pathname, searchParams} = new URL(request.url, `http://${request.headers.host}`);
@@ -70,7 +141,7 @@ const server = http.createServer((request, response) => {
         response,
         pathname,
         searchParams,
-    }
+    };
 
     method(params);
 });
